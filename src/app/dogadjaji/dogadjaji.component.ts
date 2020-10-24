e95c280542ae5c2d7da8fc7ea8691e0d1a603e57
nmihin <nmihin@gmail.com>
2020-10-24 21:05:34 +0200

    - added događaji

--- src/app/dogadjaji/dogadjaji.component.ts
+++ src/app/dogadjaji/dogadjaji.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
 
 @Component({
   selector: 'app-dogadjaji',
@@ -7,9 +8,128 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DogadjajiComponent implements OnInit {
 
-  constructor() { }
+  optionsBilanca: any;
+  private defaultColDef;
+  private gridApi;
+  private gridColumnApi;
 
-  ngOnInit(): void {
+  public modules: any[] = [ClientSideRowModelModule];
+
+  lokacije = [
+    { field: 'datum', sortable: true, filter: true  },
+    { field: 'postaja', sortable: true, filter: true },
+    { field: 'cjelina', sortable: true, filter: true },
+    { field: 'SCADA key', sortable: true, filter: true },
+    { field: 'tip podatka', sortable: true, filter: true },
+    { field: 'događaj', sortable: true, filter: true },
+    { field: 'opis događaja', sortable: true, filter: true }
+  ];
+
+  vrijednosti = [
+    {
+      'datum': '31.01.2020 10:21:23',
+      'postaja': 'CS BB',
+      'cjelina': 'Agregat',
+      'SCADA key': 'CS BB:Agregat C.431. Na mreži',
+      'tip podatka': 'MREŽA',
+      'događaj': 'Uključen',
+      'opis događaja': '---'
+    },
+    {
+      'datum': '31.01.2020 10:21:23',
+      'postaja': 'CS BB',
+      'cjelina': 'Agregat',
+      'SCADA key': 'CS BB:Agregat C.431. Na mreži',
+      'tip podatka': 'MREŽA',
+      'događaj': 'Uključen',
+      'opis događaja': '---'
+    },
+    {
+      'datum': '31.01.2020 10:21:23',
+      'postaja': 'CS BB',
+      'cjelina': 'Agregat',
+      'SCADA key': 'CS BB:Agregat C.431. Na mreži',
+      'tip podatka': 'MREŽA',
+      'događaj': 'Uključen',
+      'opis događaja': '---'
+    },
+    {
+      'datum': '31.01.2020 10:21:23',
+      'postaja': 'CS BB',
+      'cjelina': 'Agregat',
+      'SCADA key': 'CS BB:Agregat C.431. Na mreži',
+      'tip podatka': 'MREŽA',
+      'događaj': 'Uključen',
+      'opis događaja': '---'
+    },
+    {
+      'datum': '31.01.2020 10:21:23',
+      'postaja': 'CS BB',
+      'cjelina': 'Agregat',
+      'SCADA key': 'CS BB:Agregat C.431. Na mreži',
+      'tip podatka': 'MREŽA',
+      'događaj': 'Uključen',
+      'opis događaja': '---'
+    },
+    {
+      'datum': '31.01.2020 10:21:23',
+      'postaja': 'CS BB',
+      'cjelina': 'Agregat',
+      'SCADA key': 'CS BB:Agregat C.431. Na mreži',
+      'tip podatka': 'MREŽA',
+      'događaj': 'Uključen',
+      'opis događaja': '---'
+    },
+    {
+      'datum': '31.01.2020 10:21:23',
+      'postaja': 'CS BB',
+      'cjelina': 'Agregat',
+      'SCADA key': 'CS BB:Agregat C.431. Na mreži',
+      'tip podatka': 'MREŽA',
+      'događaj': 'Uključen',
+      'opis događaja': '---'
+    },
+    {
+      'datum': '31.01.2020 10:21:23',
+      'postaja': 'CS BB',
+      'cjelina': 'Agregat',
+      'SCADA key': 'CS BB:Agregat C.431. Na mreži',
+      'tip podatka': 'MREŽA',
+      'događaj': 'Uključen',
+      'opis događaja': '---'
+    },
+    {
+      'datum': '31.01.2020 10:21:23',
+      'postaja': 'CS BB',
+      'cjelina': 'Agregat',
+      'SCADA key': 'CS BB:Agregat C.431. Na mreži',
+      'tip podatka': 'MREŽA',
+      'događaj': 'Uključen',
+      'opis događaja': '---'
+    }
+  ];
+
+  constructor() {
+    this.defaultColDef = {
+      width: 150,
+      resizable: true,
+    };
   }
+
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    params.api.sizeColumnsToFit();
+    window.addEventListener('resize', function () {
+      setTimeout(function () {
+        params.api.sizeColumnsToFit();
+      });
+    });
+
+    params.api.sizeColumnsToFit();
+  }
+
+  ngOnInit(): void {}
 
 }
