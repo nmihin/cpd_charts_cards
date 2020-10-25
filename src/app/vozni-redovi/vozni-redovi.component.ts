143c6f208ccb0d35b4b1dfde70c7c2f28788e4b2
nmihin <nmihin@gmail.com>
2020-10-25 10:40:00 +0100

    - vozni redovi

--- src/app/vozni-redovi/vozni-redovi.component.ts
+++ src/app/vozni-redovi/vozni-redovi.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
 
 @Component({
   selector: 'app-vozni-redovi',
@@ -7,7 +8,209 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VozniRedoviComponent implements OnInit {
 
-  constructor() { }
+  private defaultColDef;
+  private gridApi;
+  private gridColumnApi;
+
+  public modules: any[] = [ClientSideRowModelModule];
+
+  private currentRowHeight;
+  private getRowHeight;
+
+  polja = [
+    { field: 'Vrijeme', sortable: true, filter: true  },
+    { field: 'Podsliv Rječina: Dotok (m3/s)', sortable: true, filter: true },
+    { field: 'Sliv Rječina: Snaga (MW)', sortable: true, filter: true },
+    { field: 'HE Rijeka: Snaga (MW)', sortable: true, filter: true },
+    { field: 'HE Rijeka Ag1: Snaga (MW)', sortable: true, filter: true },
+    { field: 'HE Rijeka Ag2: Snaga (MW)', sortable: true, filter: true },
+    { field: 'Akumulacija Valići: Preljev (m3/s)', sortable: true, filter: true },
+    { field: 'Akumulacija Valići: Razina (m.n.m.)', sortable: true, filter: true },
+    { field: 'HE Rijeka: Protok (m3/s)', sortable: true, filter: true },
+    { field: 'HE Rijeka Ag1: Protok (m3/s)', sortable: true, filter: true },
+    { field: 'HE Rijeka Ag2: Protok (m3/s)', sortable: true, filter: true }
+  ];
+
+  vrijednosti = [
+    {
+      'Vrijeme': '01.05.2019. 12:00',
+      'Podsliv Rječina: Dotok (m3/s)': 0,
+      'Sliv Rječina: Snaga (MW)': 0,
+      'HE Rijeka: Snaga (MW)': 0,
+      'HE Rijeka Ag1: Snaga (MW)': 0,
+      'HE Rijeka Ag2: Snaga (MW)': '10,05',
+      'Akumulacija Valići: Preljev (m3/s)': 0,
+      'Akumulacija Valići: Razina (m.n.m.)': 0,
+      'HE Rijeka: Protok (m3/s)': 0,
+      'HE Rijeka Ag1: Protok (m3/s)': 0,
+      'HE Rijeka Ag2: Protok (m3/s)': 0
+    },
+    {
+      'Vrijeme': '01.05.2019. 12:00',
+      'Podsliv Rječina: Dotok (m3/s)': 0,
+      'Sliv Rječina: Snaga (MW)': 0,
+      'HE Rijeka: Snaga (MW)': 0,
+      'HE Rijeka Ag1: Snaga (MW)': 0,
+      'HE Rijeka Ag2: Snaga (MW)': '10,05',
+      'Akumulacija Valići: Preljev (m3/s)': 0,
+      'Akumulacija Valići: Razina (m.n.m.)': 0,
+      'HE Rijeka: Protok (m3/s)': 0,
+      'HE Rijeka Ag1: Protok (m3/s)': 0,
+      'HE Rijeka Ag2: Protok (m3/s)': 0
+    },
+    {
+      'Vrijeme': '01.05.2019. 12:00',
+      'Podsliv Rječina: Dotok (m3/s)': 0,
+      'Sliv Rječina: Snaga (MW)': 0,
+      'HE Rijeka: Snaga (MW)': 0,
+      'HE Rijeka Ag1: Snaga (MW)': 0,
+      'HE Rijeka Ag2: Snaga (MW)': '10,05',
+      'Akumulacija Valići: Preljev (m3/s)': 0,
+      'Akumulacija Valići: Razina (m.n.m.)': 0,
+      'HE Rijeka: Protok (m3/s)': 0,
+      'HE Rijeka Ag1: Protok (m3/s)': 0,
+      'HE Rijeka Ag2: Protok (m3/s)': 0
+    },
+    {
+      'Vrijeme': '01.05.2019. 12:00',
+      'Podsliv Rječina: Dotok (m3/s)': 0,
+      'Sliv Rječina: Snaga (MW)': 0,
+      'HE Rijeka: Snaga (MW)': 0,
+      'HE Rijeka Ag1: Snaga (MW)': 0,
+      'HE Rijeka Ag2: Snaga (MW)': '10,05',
+      'Akumulacija Valići: Preljev (m3/s)': 0,
+      'Akumulacija Valići: Razina (m.n.m.)': 0,
+      'HE Rijeka: Protok (m3/s)': 0,
+      'HE Rijeka Ag1: Protok (m3/s)': 0,
+      'HE Rijeka Ag2: Protok (m3/s)': 0
+    },
+    {
+      'Vrijeme': '01.05.2019. 12:00',
+      'Podsliv Rječina: Dotok (m3/s)': 0,
+      'Sliv Rječina: Snaga (MW)': 0,
+      'HE Rijeka: Snaga (MW)': 0,
+      'HE Rijeka Ag1: Snaga (MW)': 0,
+      'HE Rijeka Ag2: Snaga (MW)': '10,05',
+      'Akumulacija Valići: Preljev (m3/s)': 0,
+      'Akumulacija Valići: Razina (m.n.m.)': 0,
+      'HE Rijeka: Protok (m3/s)': 0,
+      'HE Rijeka Ag1: Protok (m3/s)': 0,
+      'HE Rijeka Ag2: Protok (m3/s)': 0
+    },
+    {
+      'Vrijeme': '01.05.2019. 12:00',
+      'Podsliv Rječina: Dotok (m3/s)': 0,
+      'Sliv Rječina: Snaga (MW)': 0,
+      'HE Rijeka: Snaga (MW)': 0,
+      'HE Rijeka Ag1: Snaga (MW)': 0,
+      'HE Rijeka Ag2: Snaga (MW)': '10,05',
+      'Akumulacija Valići: Preljev (m3/s)': 0,
+      'Akumulacija Valići: Razina (m.n.m.)': 0,
+      'HE Rijeka: Protok (m3/s)': 0,
+      'HE Rijeka Ag1: Protok (m3/s)': 0,
+      'HE Rijeka Ag2: Protok (m3/s)': 0
+    },
+    {
+      'Vrijeme': '01.05.2019. 12:00',
+      'Podsliv Rječina: Dotok (m3/s)': 0,
+      'Sliv Rječina: Snaga (MW)': 0,
+      'HE Rijeka: Snaga (MW)': 0,
+      'HE Rijeka Ag1: Snaga (MW)': 0,
+      'HE Rijeka Ag2: Snaga (MW)': '10,05',
+      'Akumulacija Valići: Preljev (m3/s)': 0,
+      'Akumulacija Valići: Razina (m.n.m.)': 0,
+      'HE Rijeka: Protok (m3/s)': 0,
+      'HE Rijeka Ag1: Protok (m3/s)': 0,
+      'HE Rijeka Ag2: Protok (m3/s)': 0
+    },
+    {
+      'Vrijeme': '01.05.2019. 12:00',
+      'Podsliv Rječina: Dotok (m3/s)': 0,
+      'Sliv Rječina: Snaga (MW)': 0,
+      'HE Rijeka: Snaga (MW)': 0,
+      'HE Rijeka Ag1: Snaga (MW)': 0,
+      'HE Rijeka Ag2: Snaga (MW)': '10,05',
+      'Akumulacija Valići: Preljev (m3/s)': 0,
+      'Akumulacija Valići: Razina (m.n.m.)': 0,
+      'HE Rijeka: Protok (m3/s)': 0,
+      'HE Rijeka Ag1: Protok (m3/s)': 0,
+      'HE Rijeka Ag2: Protok (m3/s)': 0
+    },
+    {
+      'Vrijeme': '01.05.2019. 12:00',
+      'Podsliv Rječina: Dotok (m3/s)': 0,
+      'Sliv Rječina: Snaga (MW)': 0,
+      'HE Rijeka: Snaga (MW)': 0,
+      'HE Rijeka Ag1: Snaga (MW)': 0,
+      'HE Rijeka Ag2: Snaga (MW)': '10,05',
+      'Akumulacija Valići: Preljev (m3/s)': 0,
+      'Akumulacija Valići: Razina (m.n.m.)': 0,
+      'HE Rijeka: Protok (m3/s)': 0,
+      'HE Rijeka Ag1: Protok (m3/s)': 0,
+      'HE Rijeka Ag2: Protok (m3/s)': 0
+    },
+    {
+      'Vrijeme': '01.05.2019. 12:00',
+      'Podsliv Rječina: Dotok (m3/s)': 0,
+      'Sliv Rječina: Snaga (MW)': 0,
+      'HE Rijeka: Snaga (MW)': 0,
+      'HE Rijeka Ag1: Snaga (MW)': 0,
+      'HE Rijeka Ag2: Snaga (MW)': '10,05',
+      'Akumulacija Valići: Preljev (m3/s)': 0,
+      'Akumulacija Valići: Razina (m.n.m.)': 0,
+      'HE Rijeka: Protok (m3/s)': 0,
+      'HE Rijeka Ag1: Protok (m3/s)': 0,
+      'HE Rijeka Ag2: Protok (m3/s)': 0
+    },
+    {
+      'Vrijeme': '01.05.2019. 12:00',
+      'Podsliv Rječina: Dotok (m3/s)': 0,
+      'Sliv Rječina: Snaga (MW)': 0,
+      'HE Rijeka: Snaga (MW)': 0,
+      'HE Rijeka Ag1: Snaga (MW)': 0,
+      'HE Rijeka Ag2: Snaga (MW)': '10,05',
+      'Akumulacija Valići: Preljev (m3/s)': 0,
+      'Akumulacija Valići: Razina (m.n.m.)': 0,
+      'HE Rijeka: Protok (m3/s)': 0,
+      'HE Rijeka Ag1: Protok (m3/s)': 0,
+      'HE Rijeka Ag2: Protok (m3/s)': 0
+    },
+    {
+      'Vrijeme': '01.05.2019. 12:00',
+      'Podsliv Rječina: Dotok (m3/s)': 0,
+      'Sliv Rječina: Snaga (MW)': 0,
+      'HE Rijeka: Snaga (MW)': 0,
+      'HE Rijeka Ag1: Snaga (MW)': 0,
+      'HE Rijeka Ag2: Snaga (MW)': '10,05',
+      'Akumulacija Valići: Preljev (m3/s)': 0,
+      'Akumulacija Valići: Razina (m.n.m.)': 0,
+      'HE Rijeka: Protok (m3/s)': 0,
+      'HE Rijeka Ag1: Protok (m3/s)': 0,
+      'HE Rijeka Ag2: Protok (m3/s)': 0
+    }
+  ];
+
+  constructor() {
+
+    this.defaultColDef = { resizable: true };
+    this.getRowHeight = function () {
+      return this.currentRowHeight;
+    };
+  }
+
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    params.api.sizeColumnsToFit();
+    window.addEventListener('resize', function () {
+      setTimeout(function () {
+        params.api.sizeColumnsToFit();
+      });
+    });
+
+    params.api.sizeColumnsToFit();
+  }
 
   ngOnInit(): void {
   }
